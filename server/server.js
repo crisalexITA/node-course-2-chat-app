bb3d461bdbe09a1a736a1f24fb220806e7dac567
cristian alessandrini <cristian.alessandrini@outlook.com>
2018-12-01 10:51:41 +0100

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -13,18 +15,10 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     // socket.emit from Admin text Welcome to the chat app
-    socket.emit('newMessage',{
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     // socket.broadcast.emit from Admin text New user joined
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
     socket.on('disconnect', () => {
         console.log('User was disconnected');
@@ -36,11 +30,7 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
---
+++ server/test/message.test.js
@@ -0,0 +1,12 @@
+var expect = require('expect');
+var {generateMessage} = require('../utils/message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'Jen';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text});
+    })
+});
