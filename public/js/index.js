6409d8370def905eb800a97c653513fd46bf480d
cristian alessandrini <cristian.alessandrini@outlook.com>
2018-12-30 15:03:08 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,23 @@
 var socket = io();
+
+function scrollToBottom () {
+
+    // selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
@@ -14,6 +33,7 @@ socket.on('newMessage', function(message){
         createdAt: formattedTime 
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
     // console.log('New message', message);
     
     // var li = jQuery('<li></li>');
@@ -30,6 +50,7 @@ socket.on('newLocationMessage', function(message){
         createdAt: formattedTime
     });
     Jquery('#messages').append(html);
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a>');
     // li.text(message.from + ' ' + formattedTime + ':');
